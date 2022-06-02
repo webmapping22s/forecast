1b3392c2edd4b59e520f8ed8901e94e7afad8187
Klaus Förster <klaus.foerster@uibk.ac.at>
2022-06-02 16:03:10 +0200

    Vorhersagedatum berechnet

--- main.js
+++ main.js
@@ -32,7 +32,14 @@ L.control.scale({
 async function loadWind(url) {
     const response = await fetch(url);
     const jsondata = await response.json();
-    console.log(jsondata);
+    console.log("Jsondaten", jsondata);
+    console.log("Zeitpunkt Erstellung", jsondata[0].header.refTime);
+    console.log("Zeitpunkt Vorhersage (+Stunden)", jsondata[0].header.forecastTime);
+
+    let forecastDate = new Date(jsondata[0].header.refTime);
+    console.log("Echtes Datum Erstellung", forecastDate);
+    forecastDate.setHours(forecastDate.getHours() + jsondata[0].header.forecastTime);
+    console.log("Echtes Datum Vorhersage", forecastDate);
 };
 loadWind("https://geographie.uibk.ac.at/webmapping/ecmwf/data/wind-10u-10v-europe.json");
 
