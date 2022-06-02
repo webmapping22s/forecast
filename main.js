c2e3551b2eb32a2717a2087dfe9f13ec4fe5226d
Klaus Förster <klaus.foerster@uibk.ac.at>
2022-06-02 17:11:42 +0200

    Overlay und Marker Code außerhalb von loadWeather schreiben

--- main.js
+++ main.js
@@ -72,15 +72,15 @@ async function loadWind(url) {
 loadWind("https://geographie.uibk.ac.at/webmapping/ecmwf/data/wind-10u-10v-europe.json");
 
 // Wettervorhersage
+layerControl.addOverlay(overlays.weather, "Wettervorhersage met.no");
+
+let marker = L.circleMarker([
+    47.267222, 11.392778
+]).bindPopup("Wettervorhersage").addTo(overlays.weather);
+
 async function loadWeather(url) {
     const response = await fetch(url);
     const jsondata = await response.json();
     //console.log("Jsondaten", jsondata);
-
-    layerControl.addOverlay(overlays.weather, "Wettervorhersage met.no");
-
-    let marker = L.circleMarker([
-        47.267222, 11.392778
-    ]).bindPopup("Wettervorhersage").addTo(overlays.weather);
 };
 loadWeather("https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=47.267222&lon=11.392778");
