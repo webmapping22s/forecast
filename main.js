06e815b41298c1150d5d8c0c9e7de59502220d00
Klaus Förster <klaus.foerster@uibk.ac.at>
2022-06-02 17:09:40 +0200

    Wettervorhersage Overlay mit Marker und Popup

--- main.js
+++ main.js
@@ -77,5 +77,10 @@ async function loadWeather(url) {
     const jsondata = await response.json();
     //console.log("Jsondaten", jsondata);
 
+    layerControl.addOverlay(overlays.weather, "Wettervorhersage met.no");
+
+    let marker = L.circleMarker([
+        47.267222, 11.392778
+    ]).bindPopup("Wettervorhersage").addTo(overlays.weather);
 };
 loadWeather("https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=47.267222&lon=11.392778");
