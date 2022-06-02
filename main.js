e19bcba6c5d8574024b7e15d5397c93c2cb05343
Klaus Förster <klaus.foerster@uibk.ac.at>
2022-06-02 15:51:00 +0200

    Windvorhersagedaten asynchron laden und anzeigen

--- main.js
+++ main.js
@@ -30,7 +30,9 @@ L.control.scale({
 
 // Windvorhersage
 async function loadWind(url) {
-
+    const response = await fetch(url);
+    const jsondata = await response.json();
+    console.log(jsondata);
 };
 loadWind("https://geographie.uibk.ac.at/webmapping/ecmwf/data/wind-10u-10v-europe.json");
 
